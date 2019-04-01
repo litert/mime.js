ba57fa46dc16675f585fcedd9e7d99f3e289d3a9
yunboo <zohegs@gmail.com>
2019-04-01 20:26:19 +0800

    update.

--- sources/index.ts
+++ sources/index.ts
@@ -1,7 +1,6 @@
 let extJson = require(__dirname + "/../mime.json");
 
-export function getMIMEType(fpath: string): string {
+export function get(fpath: string): string {
     let ext = fpath.indexOf(".") === -1 ? fpath : fpath.slice(fpath.lastIndexOf(".") + 1);
-    console.log(ext);
     return extJson[ext] || "application/octet-stream";
 }
